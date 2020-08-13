907dc39f9dd7d42fff9951afebd6dcac3027d10e
407ho <69379912+407ho@users.noreply.github.com>
2020-08-13 20:08:09 +0900

    day2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {Button, Progress} from 'semantic-ui-react'
+import {Button, Input} from 'semantic-ui-react'
 
-function App() {
+
+
+import 펭귄 from "./펭귄.png"
+
+
+
+
+function Login2(){
+
+  return(
+    <div>
+    <Input/>
+    <Button primary onClick = {(e) => {
+      console.log(e)
+      alert(e.target.value)
+    } } > 로그인 </Button>
+    </div>
+
+  )
+}
+
+
+
+  class Login extends React.Component{
+    constructor(){
+      super()
+      this.state = {id : "뭐"}
+    }
+
+
+    render(){
+      return(
+        <div>
+        <Input onChange = {(e) => this.setState({id : e.target.value})}/>
+        <Button value = {this.state.id} primary onClick = {(e) => {
+          console.log(e)
+          alert(e.target.value)
+        } } > Login </Button>
+        </div>
+
+      )
+    }
+  }
+
+
+
+
+
+
+
+function App(){
   return (
     <div>
-    <p> 안녕하세요 ! </p>
-    <Button Primary> hello world </Button>
-    <Progress percent = {66} indicating/>
+    <p> 난 펭귄 아냐 </p>
+    <img src = {펭귄} style ={ {"width" : 300, "hight" : 300} }/>
+    <Button Primary> hello stranger </Button>
+    <Login/>
     </div>
   );
 }
